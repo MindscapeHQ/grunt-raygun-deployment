d38d31c039d9c9feb129f2c26e63ac9735cd89d4
Daniel Wylie <daniel@mindscape.co.nz>
2015-07-01 10:54:24 +1200

    Add an example for grunt file deployment info

---
+++ example/gruntreleaseinfo/Gruntfile.js
@@ -0,0 +1,36 @@
+/*
+ * grunt-raygun-deployment-example
+ * https://raygun.io
+ *
+ * Copyright (c) 2015 Raygun.io
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    // Configuration to be run (and then tested).
+    raygun_deployment: {
+      options: {
+        // You need to fill this in with your own data
+        // Alternatively, set the RAYGUN_APIKEY and RAYGUN_AUTHTOKEN environment variables
+        raygunApiKey: 'YOUR APPLICATIONS API KEY',
+        raygunAuthToken: 'YOUR EXTERNAL AUTH TOKEN'
+        release: {
+          version: '6.1.0.0',
+          ownerName: 'Jamie Penney',
+          emailAddress: 'jamie@example.com',
+          notes: '#Testing out the grunt plugin\n* List item'
+        }
+      }
+    }
+  });
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['raygun_deployment'])
+};
